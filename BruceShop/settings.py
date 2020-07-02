eb00236f99d3b8dcb178fd0f8e56d9f507757ba4
Dell <1419517126@qq.com>
2020-07-02 17:22:50 +0800

    初始化2

--- BruceShop/settings.py
+++ BruceShop/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'bruceshop',
         'USER':'root',
-        'PASSWORD':'12345678',
+        'PASSWORD':'123456',
         'HOST':'127.0.0.1',
         'PORT':'3306',
         'OPTIONS':{'init_command':'SET default_storage_engine=INNODB;'}
